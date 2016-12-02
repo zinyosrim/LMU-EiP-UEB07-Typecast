37659df91118995cbf9117ea8726f929fa92f3e1
Zin Yosrim <zinyosrim@gmail.com>
2016-12-02 11:29:29 +0100

    init

---
+++ src/TerminierungVonSchleifen.java
@@ -0,0 +1,51 @@
+
+public class TerminierungVonSchleifen {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+/* (a)
+		int i = 0;
+		int j = 0;
+		do {
+			j = j+ 1;
+			i = j + i;
+		} while (i < 200);
+	
+*/
+/* (b)
+		double i = 0.5;
+		double j = 0;
+		int z = 0;
+		while (j < 1) {	
+			j += i;
+			i*= 0.5;
+			z++;
+		}
+*/
+/* (c)
+			int z = 0;
+			char i = 'a';
+			short j = 0;
+			while ( i!= j) {
+				i++;
+				j = (short) (i+i);
+				z++;
+				
+				System.out.println("j = "+j+" i = "+i+" ("+(i+0)+") "+" z = "+z);
+				
+			}
+*/
+		long i = 26L;
+		long j = 24L;
+		int z = 0;
+		for (long x = 0L; x < 1000L; x++) {
+			i = i / 12 + 23 * (--x);
+			j = (x--) + j + 5;
+			z++;
+			System.out.println("j = "+j+" i = "+i+" z = "+z);
+		}
+		System.out.println("j = "+j);
+		System.out.println("i = "+i);
+		System.out.println("z = "+z);
+	}
+}
---
+++ src/Typecasting.java
@@ -0,0 +1,34 @@
+
+//EiP Übungsblatt 7, Aufgabe 7-1
+
+public class Typecasting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 9, y = 12, z = 16;
+		// (a)
+		//System.out.println(10/4);
+		//System.out.println(10/4);
+		//System.out.println(x == y ? x < y : y > z);
+		//System.out.println(false && true);
+		//System.out.println(1 || 0);
+		//System.out.println((byte)(127+1));
+		//System.out.println('x'+'y');
+		//System.out.println(x+y+"z");
+		
+		// (b)
+		//System.out.println((x>y) < true);
+		System.out.println((long)(3f/4.0));
+		System.out.println((int)4/5.0D);
+		System.out.println((long)(3F) /4.0);
+		System.out.println((float) (3F/4.0));
+		System.out.println((int)4/5.0D);
+		System.out.println((char)65537);
+		System.out.println(1L+(float)3d);
+		System.out.println('4'+0);
+		System.out.println('7'+0);	
+		System.out.println('4'+'7');
+		System.out.println('a'+3);
+	}
+
+}
